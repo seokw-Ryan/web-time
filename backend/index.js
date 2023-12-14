983097be8729ccb856d6aedd6e5b3a80eb10745b
Seokwoo Chung (Ryan) <89856141+seokw-Ryan@users.noreply.github.com>
2023-12-14 11:10:26 +0900

    work from yesterday: add new package.json and index.html

--- backend/index.js
+++ backend/index.js
@@ -10,3 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.sendFile("index.html");
 });
+
+app.post("/submit", (req, res) => {
+  const randomAdj = adj[Math.floor(Math.random() * adj.length)];
+  const randomNoun = noun[Math.floor(Math.random() * noun.length)];
+  res.render("solution.ejs", {
+    adjective: randomAdj,
+    noun: randomNoun,
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`);
+});
