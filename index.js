da873b5b9300eff62b426e6f8f24f5d42fe248c8
Seokwoo Chung (Ryan) <89856141+seokw-Ryan@users.noreply.github.com>
2023-12-27 15:39:07 +0900

    edit index.js to have basic render function

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+const yourUsername = "";
+const yourPassword = "";
+const yourAPIKey = "";
+const yourBearerToken = "";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
