e8ae49a902f9bc48e256b1379e4a02393fb7d4ef
Seokwoo Chung (Ryan) <89856141+seokw-Ryan@users.noreply.github.com>
2023-12-27 10:36:15 +0900

    work from last time: modification to the structure of the project

---
+++ index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
