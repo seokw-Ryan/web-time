9860e791ac59b1f90cc3ab14bc59228830561dc9
Seokwoo Chung (Ryan) <89856141+seokw-Ryan@users.noreply.github.com>
2024-01-02 12:03:14 +0900

    create the password page (backend)

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+var userIsAuthorised = false;
 
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { content: "API Response." });
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
