cb01a7a7284e8744cedcd447470a0aa2cc3eee01
ggg <seokwoo.chung130@gmail.com>
2024-05-29 17:41:47 +0900

    modify docker compose so that it works with frontend and backend. Currently does not load correct files but successfully builds the docker image.

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'your_db_user',
-    host: 'localhost',
-    database: 'your_db_name',
-    password: 'your_db_password',
-    port: 5432,
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
 });
 
 module.exports = pool;
